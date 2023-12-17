b80a4291af12fd661e92c0484c315acd776a0767
unknown <mck0391@gmail.com>
2023-12-17 17:47:19 +0000

    first commit

--- custom-radio-logger.php
+++ custom-radio-logger.php
@@ -13,3 +13,15 @@ function add_scripts() {
 
 // Hook the function to the wp_enqueue_scripts action
 add_action('wp_enqueue_scripts', 'add_scripts');
+
+// Shortcode function to display the radio button
+function custom_radio_logger_shortcode() {
+    ob_start(); ?>
+
+    <?php
+    $output = ob_get_clean();
+    return $output;
+}
+
+// Register the shortcode
+add_shortcode('custom_radio_logger', 'custom_radio_logger_shortcode');
