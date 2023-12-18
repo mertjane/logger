6689886af37d9fcd1459f544b93dde0db7f5bc61
unknown <mck0391@gmail.com>
2023-12-18 12:29:56 +0000

    first commit

--- custom-radio-logger.js
+++ custom-radio-logger.js
@@ -50,58 +50,44 @@
 
 jQuery(document).ready(function ($) {
 
-    // Listen to the radio buttons of the WPC Variations Radio Buttons plugin
-    $('.woovr-variation-radio').on('click', function () {
-        var selectedVariation = $(this).data('id');
-        var sizeName = '';
+     // Attach input event listener to squareMeterInput
+     $("#squareMeterInput").on("input", function(){
+        // Remove decimal part if entered
+        $(this).val(parseInt($(this).val()));
+        
+        // Get the entered square meters
+        var squareMeters = $(this).val();
 
         // Get the product size name of the selected variation.
-        if (selectedVariation !== 0) {
-            sizeName = $(this).find('.woovr-variation-name').text();
-        }
+        var sizeName = '';
 
-        console.log('Selected radio button value:', sizeName);
-    });
-
-    // Listen to changes in the square meter input field
-    $('#squareMeterInput').on('input', function () {
-        var squareMeterValue = $(this).val();
-        squareMeterValue = squareMeterValue.replace(/\.\d+/g, '');
-        $(this).val(squareMeterValue);
-        console.log('Square Meter Input:', squareMeterValue);
-
-        // Get the selected dimension (sizeName) from the radio button
-        var selectedDimension = getSelectedDimension();
-
-        // Calculate quantity based on square meter input and selected dimension
-        if (squareMeterValue !== '' && !isNaN(squareMeterValue) && selectedDimension) {
-            var dimensions = selectedDimension.split('x');
-            var length = parseFloat(dimensions[0]);
-            var width = parseFloat(dimensions[1]);
-
-            var quantity = Math.round((parseFloat(squareMeterValue) / (length * width)) * 100) / 100;
-
-            // Update the quantity input field
-            $('#tilePieceInput').val(quantity.toFixed(0));
-        }
-
-        // Update any other fields as needed
-        // ...
-    });
-
-    // Function to get the selected dimension (sizeName) from the radio button
-    function getSelectedDimension() {
-        var selectedDimension = '';
-
-        $('.woovr-variation-radio').each(function () {
-            if ($(this).hasClass('woovr-variation-radio-selected')) {
-                selectedDimension = $(this).find('.woovr-variation-name').text();
-                return false; // Break the loop when the selected radio button is found
+        // Listen for radio button clicks
+        $('.woovr-variation-radio').on('click', function () {
+            var selectedVariation = $(this).data('id');
+            
+            // Get the product size name of the selected variation.
+            if (selectedVariation !== 0) {
+                sizeName = $(this).find('.woovr-variation-name').text();
             }
+
+            // Update tile dimensions based on the selected variation
+            updateTileDimensions(sizeName);
         });
 
-        return selectedDimension;
-    }
+        // Function to update tile dimensions based on the selected variation
+        function updateTileDimensions(sizeName) {
+            // Extract width and height from the sizeName (assuming the format is "widthxheight")
+            var dimensions = sizeName.split('x');
+            var tileWidth = parseInt(dimensions[0]);
+            var tileHeight = parseInt(dimensions[1]);
+
+            // Calculate the number of tiles needed
+            var tilesNeeded = Math.ceil((squareMeters * 1000000) / (tileWidth * tileHeight));
+
+            // Update the tilePieceInput with the calculated quantity
+            $("#tilePieceInput").val(tilesNeeded);
+        }
+    });
 });
 
 
