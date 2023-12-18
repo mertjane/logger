fe37ca437e50db11e6c0af4e9184a8f6c5c08faf
unknown <mck0391@gmail.com>
2023-12-18 11:25:10 +0000

    first commit

--- custom-radio-logger.js
+++ custom-radio-logger.js
@@ -1,10 +1,7 @@
-jQuery(document).ready(function($) {
-    // Wait for the DOM to be ready
-    // $('.product-size input').prop("readonly", true);
-    // $('.product-size').prepend('Size:');
+jQuery(document).ready(function ($) {
 
     // Listen to the radio buttons of the WPC Variations Radio Buttons plugin
-    $('.woovr-variation-radio').on('click', function() {
+    $('.woovr-variation-radio').on('click', function () {
         var selectedVariation = $(this).data('id');
         var sizeName = '';
 
@@ -13,54 +10,36 @@ jQuery(document).ready(function($) {
             sizeName = $(this).find('.woovr-variation-name').text();
         }
 
-        // console.log('Selected radio button value:', sizeName);
-
-        // Calculate tiles based on square meter input
-        calculateTiles(sizeName);
+        console.log('Selected radio button value:', sizeName);
     });
 
-    // Function to round a number to the nearest integer
-    function roundToInteger(number) {
-        return Math.round(number);
-    }
-
-    // Event handler for square meter input change
+    // Listen to changes in the square meter input field
     $('#squareMeterInput').on('input', function () {
-        updateCalculation();
+        var squareMeterValue = $(this).val();
+        console.log('Square Meter Input:', squareMeterValue);
+
+        // Your existing calculation logic here
+
+        // Update the quantity input field based on the calculation
+        // ...
+
+        // Update any other fields as needed
+        // ...
     });
 
-    // Event handler for tile piece input change
-    $('#tilePieceInput').on('keydown', function (e) {
-        // Prevent entering decimals
-        if (e.key === '.' || e.key === ',') {
-            e.preventDefault();
-        }
-    });
-
-    // Event handler for tile piece input change
+    // Listen to changes in the tile piece input field
     $('#tilePieceInput').on('input', function () {
-        updateCalculation();
+        var tilePieceValue = $(this).val();
+        console.log('Tile Piece Input:', tilePieceValue);
+
+        // Your existing calculation logic here
+
+        // Update the square meter input field based on the calculation
+        // ...
+
+        // Update any other fields as needed
+        // ...
     });
-
-
-    // Function to update the calculation based on inputs
-    function updateCalculation(sizeName) {
-        var squareMeterValue = parseFloat($('#squareMeterInput').val());
-        var sizeName = $('.woovr-variation-radio.selected').find('.woovr-variation-name').text();
-    
-        // Parse the dimensions (assuming format: 305x305x12 mm)
-        var dimensions = sizeName.split('x');
-        var length = parseFloat(dimensions[0]);
-        var width = parseFloat(dimensions[1]);
-        var thickness = parseFloat(dimensions[2]);
-
-        if (!isNaN(squareMeterValue) && squareMeterValue > 0 && !isNaN(length) && length > 0 && !isNaN(width) && width > 0) {
-            var tileQuantity = roundToInteger(squareMeterValue / ((length / 1000) * (width / 1000))); // Convert size to meters
-            $('#tilePieceInput').val(tileQuantity);
-        } else {
-            $('#tilePieceInput').val('');
-        }
-    }    
 });
 
 
